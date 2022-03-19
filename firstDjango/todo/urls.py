336065f135eb7eac4f4dbc88232e67bb8e5f18b8
DESKTOP-GTIIL4S\Work <sodam138@naver.com>
2022-03-20 06:55:55 +0900

    프론트앤드 추가 + 렌더링 api 추가

--- firstDjango/todo/urls.py
+++ firstDjango/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path as url
-from .views import TaskCreate, TaskSelect
+from .views import TaskCreate, TaskSelect, Todo
 
 urlpatterns = [
     url('create',TaskCreate.as_view(),name= 'create'),
-    url('select',TaskSelect.as_view(),name= 'select')
+    url('select',TaskSelect.as_view(),name= 'select'),
+    url('',Todo.as_view(),name= 'todo')
 ]
--- firstDjango/todo/views.py
+++ firstDjango/todo/views.py
@@ -2,6 +2,33 @@ from django.db.models.functions import datetime
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Task
+from django.shortcuts import render
+
+class Todo(APIView):
+    def post(self, request):
+        user_id = request.data.get('user_id', "")
+        name = request.data.get('name', "")
+        end_date = request.data.get('end_date', None)
+        if end_date:
+            end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        Task.objects.create(user_id=user_id, task_name=name, end_date=end_date)
+
+        tasks = Task.objects.all()
+        task_list = []
+        for task in tasks:
+            task_list.append(dict(task_name=task.task_name, start_date=task.start_date, end_date=task.end_date, state=task.state))
+        context = dict(task_list=task_list)
+        return render(request, 'todo/todo.html', context=context)
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        task_list = []
+        for task in tasks:
+            task_list.append(dict(task_name=task.task_name, start_date=task.start_date, end_date=task.end_date, state=task.state))
+        context=dict(task_list=task_list)
+        return render(request, 'todo/todo.html', context=context)
+
+
 
 class TaskCreate(APIView):
     def post(self, request):
@@ -10,7 +37,7 @@ class TaskCreate(APIView):
         end_date = request.data.get("end_date",None)
 
         if end_date:
-            end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+            end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
         task = Task.objects.create(user_id=user_id, task_name=task_name, end_date=end_date)
 
         data = dict(msg="Todo 생성완료", task_name=task.task_name,\
